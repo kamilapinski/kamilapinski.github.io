e4451f370a39e209a7a7c74eabf7169e787d9890
Kamil Łapiński <kamill2004@gmail.com>
2025-10-30 15:49:50 +0100

    fix

--- wkp/scripts/results.js
+++ wkp/scripts/results.js
@@ -2,6 +2,7 @@ const questionContainers = document.querySelectorAll('.question-container');
 
 function showQuestion(questionNumber) {
     questionContainers.forEach((container, index) => {
+        console.log('Index:', index, 'Question number:', questionNumber);
         container.style.display = index === questionNumber ? 'block' : 'none';
     });
 }
